10a8a022a026267d3d15f1ce373dd889e2356ec8
Aman Singh <aman.singh@sts.in>
2019-05-13 19:39:51 +0530

    Added the babel-preset-stage- to use es6 syntex in class component

--- src/components/HelloWorld/HelloWorld.js
+++ src/components/HelloWorld/HelloWorld.js
@@ -1,12 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
 import "./HelloWorld.scss";
 
-const HelloWorld = () => {
-	return (
-		<div className="Hello">
-			HelloWorld
-		</div>
-	)
+export class HelloWorld extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			count: 0,
+			name: ''
+		};
+	}
+
+    addCount = () => {
+        this.setState({
+	      	count: this.state.count + 1
+	    });
+    };
+
+    addName = (event) => {
+    	this.setState({
+	      	name: event.target.value
+	    });
+    }
+
+    render() {
+    	const { count, name } = this.state;
+        return (
+           <div className="Hello">
+           		<a>Totle clicked: <b>{count}</b></a>
+               	<button onClick={this.addCount}>Click me</button>
+               	Please Add Your Name:
+               	<input type="text" value={name} onChange={this.addName}/>
+               	Your Name is: {name}
+           </div>
+        )
+    }
 }
 
 export default HelloWorld;
